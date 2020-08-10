c567e01349c64916088361ff0318ac03f02f315a
FirasHM2 <58623398+FirasHM2@users.noreply.github.com>
2020-08-10 17:39:34 +0200

    getting things ready for heroku

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: 'mongodb://localhost/produitsMangez-moi',
+    db: 'mongodb+srv://admin-firas:firas123@cluster0-vtlh2.mongodb.net/Mangez-moi?retryWrites=true&w=majority',
     secret: "123456789",
     CLIENT_ID : "CLIENT_ID",
     CLIENT_SECRET: "CLIENT_SECRET",
