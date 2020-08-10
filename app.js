c9bff62f6a7e0a966aae9050fa81705f0659e760
FirasHM2 <58623398+FirasHM2@users.noreply.github.com>
2020-08-10 16:07:51 +0200

    add .env Procfile and modified .getignore1

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('./config/express');
 var mongoose = require('./config/mongoose');
+const PORT = process.env.PORT || 3001
 
 var db = mongoose();
 var app = express();
 
-app.listen(process.env.PORT);
+app.listen(PORT);
 module.exports = app;
 
 console.log('Server running at http://localhost:3001/');
